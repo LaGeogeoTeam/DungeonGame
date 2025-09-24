b736c7dc5738f68deab9e2c7c8e987b355ea22c5
LaGeogeoTeam_pc <geoffrey.guillouet@epitech.eu>
2025-09-24 17:45:17 +0200

    refacto: refactorize file based on a github repository

--- classic.lua
+++ classic.lua
@@ -1,20 +1,9 @@
---
--- classic
---
--- Copyright (c) 2014, rxi
---
--- This module is free software; you can redistribute it and/or modify it under
--- the terms of the MIT license. See LICENSE for details.
---
-
-
+-- classic.lua
+-- https://github.com/rxi/classic
 local Object = {}
 Object.__index = Object
 
-
-function Object:new()
-end
-
+function Object:new() end
 
 function Object:extend()
   local cls = {}
@@ -29,7 +18,6 @@ function Object:extend()
   return cls
 end
 
-
 function Object:implement(...)
   for _, cls in pairs({...}) do
     for k, v in pairs(cls) do
@@ -40,29 +28,23 @@ function Object:implement(...)
   end
 end
 
-
 function Object:is(T)
   local mt = getmetatable(self)
   while mt do
-    if mt == T then
-      return true
-    end
+    if mt == T then return true end
     mt = getmetatable(mt)
   end
   return false
 end
 
-
 function Object:__tostring()
   return "Object"
 end
-
 
 function Object:__call(...)
   local obj = setmetatable({}, self)
   obj:new(...)
   return obj
 end
-
 
 return Object
